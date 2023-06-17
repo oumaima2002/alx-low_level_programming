d366169b1aafa9ad47b928f450435d8037e4038b
oumaima2002 <gx1783711@gmail.com>
2023-06-16 18:27:55 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+#include<stdio.h>
+
+
+/**
+ *main- Entry point
+ *Return Always 0 (Success)
+*/
+int main(void)
+{
+int i;
+for(i='0';i<='9';i++)
+putchar(i);
+putchar('\n');
+return (0);
+}
