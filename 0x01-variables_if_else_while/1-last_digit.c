be7e9fdb3ddbd2fe4942291825fc7144eace77d4
oumaima ouasli <103319672+oumaima2002@users.noreply.github.com>
2023-06-17 01:44:26 +0100

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,7 +5,7 @@
 #include<stdio.h>
 
 
-/* main- This program will assign a random number to the variable n each time it is executed */
+/* main - This program will assign a random number to the variable n each time it is executed */
 int main(void)
 {
 	int n , lastd;
