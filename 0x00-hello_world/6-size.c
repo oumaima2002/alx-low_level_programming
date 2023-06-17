d91e398bc83175ff3040d9641e9286ad2d18a7f9
oumaima2002 <gx1783711@gmail.com>
2023-06-17 12:07:49 -0700

    sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ char c;
 long int d;
 long long int e;
 
-printf("Size of a char :%lu byte(s)",sizeof(c));
-printf("Size of an int :%lu byte(s) ",sizeof(a));
-printf("Size of a long int :%lu byte(s) ",sizeof(d));
-printf("Size of a long long int :%lu byte(s) ",sizeof(e));
+printf("Size of a char :%lu byte(s) \n ",sizeof(c));
+printf("Size of an int :%lu byte(s)\n ",sizeof(a));
+printf("Size of a long int :%lu byte(s)\n  ",sizeof(d));
+printf("Size of a long long int :%lu byte(s)\n  ",sizeof(e));
 
 
 return(0);
