62fd11a38384af1ddaaddf1b9d8a6ba5c6fadbc5
oumaima2002 <gx1783711@gmail.com>
2023-06-16 17:56:43 -0700

    print-alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints alphapbet
+ * return : Always (Success)
+ */
+
+int main(void)
+{
+	char c;
+	for(c='a';c<='z';c++)
+	putchar(c);
+	putchar('\n');
+	return(0);
+}	
